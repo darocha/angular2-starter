2ba1f4324d5cced39288cf41f620aea918c1ebdf
brian880120 <bobriandai@gmail.com>
2017-01-30 23:19:28 -0500

    update

--- src/app/components/character-list/character-list.component.ts
+++ src/app/components/character-list/character-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Character } from '../character/character.model';
 import { CharacterService } from './character.service';
 
@@ -7,15 +7,20 @@ import { CharacterService } from './character.service';
     templateUrl: 'character-list.template.html',
     styles: ['li {cursor: pointer;}']
 })
-export class CharacterListComponent {
-    messages: string[] = [];
+export class CharacterListComponent implements OnInit {
     selectedCharacter: Character;
     characters: Array<Character>;
 
     constructor (
         private characterService: CharacterService
-    ) {
-        this.characters = characterService.getCharacters();
+    ) {}
+
+    ngOnInit() {
+        this.characters = this.characterService.getCharacters();
+    }
+
+    clearSelection() {
+        this.selectedCharacter = null;
     }
 
     select(character: Character) {
--- src/app/components/character/character.component.ts
+++ src/app/components/character/character.component.ts
@@ -1,10 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 import { Character } from '../character/character.model';
 
 @Component({
     selector: 'my-character',
     templateUrl: 'character.template.html'
 })
-export class CharacterComponent {
+export class CharacterComponent implements OnChanges, OnInit, AfterViewInit, OnDestroy {
     @Input() character: Character;
+
+    ngAfterViewInit() {
+        console.log(`ngAfterViewInit for ${this.character.name}`);
+    }
+
+    ngOnChanges() {
+        console.log(`ngOnChanges for ${this.character.name}`);
+    }
+
+    ngOnDestroy() {
+        console.log(`ngOnDestroy for ${this.character.name}`);
+    }
+
+    ngOnInit() {
+        console.log(`ngOnInit for ${this.character.name}`);
+    }
 }
